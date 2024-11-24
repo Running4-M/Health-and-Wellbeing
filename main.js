2b889bd80e2471c0a878f0ff5ddc986189d9a828
Running4-M <mchlrotaru@gmail.com>
2024-11-24 21:52:39 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+const obesity = document.querySelector('.obesity_js');
+const pollution = document.querySelector('.pollution_js');
+const diseases = document.querySelector('.diseases_js');
+
+obesity.addEventListener("click", ()=> {
+    window.location.href = "../Obesity/obesity.html";
+});
+pollution.addEventListener("click", ()=> {
+    window.location.href = "../Pollution/pollution.html";
+});
+diseases.addEventListener("click", ()=> {
+    window.location.href = "../Diseases/diseases.html";
+});
