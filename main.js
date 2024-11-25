acd3cc11537813bcc9cf719e01bb71f0e165c159
Running4-M <mchlrotaru@gmail.com>
2024-11-25 10:25:25 +0000

    Update main.js

--- main.js
+++ main.js
@@ -3,11 +3,11 @@ const pollution = document.querySelector('.pollution_js');
 const diseases = document.querySelector('.diseases_js');
 
 obesity.addEventListener("click", ()=> {
-    window.location.href = "../Obesity/obesity.html";
+    window.location.href = "https://running4-m.github.io/Health-and-Wellbeing/Obesity/obesity.html";
 });
 pollution.addEventListener("click", ()=> {
-    window.location.href = "../Pollution/pollution.html";
+    window.location.href = "https://running4-m.github.io/Health-and-Wellbeing/Pollution/pollution.html";
 });
 diseases.addEventListener("click", ()=> {
-    window.location.href = "./Diseases/diseases.html";
+    window.location.href = "https://running4-m.github.io/Health-and-Wellbeing/Obesity/obesity.html";
 });
